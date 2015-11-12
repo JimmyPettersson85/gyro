cf2b9da00c9ab0c66a738768d2be41be4ec7b678
Jimmy Pettersson <jimmy@expertmaker.com>
2015-11-12 16:40:24 +0100

    initial commit

---
+++ gyro.go
@@ -0,0 +1,174 @@
+package gyro
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	flags            = os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	fileMode         = 0644
+	defaultLayout    = "2006-01-02T15"
+	defaultExtension = "log"
+)
+
+// Logger holds all the state for rotating logs
+type Logger struct {
+	// path to where files are written
+	path string
+
+	// placeholders for formatting filenames
+	prefix    string
+	suffix    string
+	separator string
+	layout    string
+	extension string
+
+	// what time to use when formatting filenames
+	timeFn func() time.Time
+
+	// holds the pre-formatted string for filenames
+	format string
+
+	// protects against concurrent writes
+	mu *sync.Mutex
+}
+
+// New returns a new rotating logger with the default values.
+func New(path string) (*Logger, error) {
+	l := &Logger{
+		path:      path,
+		mu:        &sync.Mutex{},
+		layout:    defaultLayout,
+		extension: defaultExtension,
+		timeFn:    func() time.Time { return time.Now().UTC() },
+	}
+
+	// Make sure we have write permissions in l.path
+	if err := l.canWrite(); err != nil {
+		return nil, err
+	}
+
+	l.buildFormatString()
+
+	return l, nil
+}
+
+// SetPrefix sets filename prefix
+func (l *Logger) SetPrefix(prefix string) {
+	l.prefix = prefix
+	l.buildFormatString()
+}
+
+// SetSuffix sets filename suffix
+func (l *Logger) SetSuffix(suffix string) {
+	l.suffix = suffix
+	l.buildFormatString()
+}
+
+// SetSeparator sets the field separator
+func (l *Logger) SetSeparator(sep string) {
+	l.separator = sep
+	l.buildFormatString()
+}
+
+// SetLayout sets the layout passed to time.Format()
+// This controls how often the logs are rotated depending
+// on the highest resolution element in the layout string.
+func (l *Logger) SetLayout(layout string) {
+	l.layout = layout
+}
+
+// SetExtension sets the filename extension
+func (l *Logger) SetExtension(ext string) {
+	l.extension = ext
+	l.buildFormatString()
+}
+
+// SetTimeFunction sets the function that returns the time
+// passed to time.Format()
+func (l *Logger) SetTimeFunction(f func() time.Time) {
+	l.timeFn = f
+}
+
+// FileName returns the current filename
+func (l *Logger) FileName() string {
+	return fmt.Sprintf(l.format, l.timeFn().Format(l.layout))
+}
+
+// Write writes the byte data to the log file
+func (l *Logger) Write(data []byte) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	f, err := os.OpenFile(path.Join(l.path, l.FileName()), flags, fileMode)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	n, err := f.Write(data)
+	if n != len(data) {
+		return fmt.Errorf("Didnt write all data. Wrote %d out of %d bytes", n, len(data))
+	}
+
+	return err
+}
+
+// WriteString writes the data string to the log file
+func (l *Logger) WriteString(data string) error {
+	return l.Write([]byte(data))
+}
+
+// String returns a debug string of the logger
+func (l *Logger) String() string {
+	var buffer bytes.Buffer
+	buffer.WriteString("gyro.Logger\n")
+	buffer.WriteString(fmt.Sprintf("  path: %s\n", l.path))
+	buffer.WriteString(fmt.Sprintf("  prefix: %q\n", l.prefix))
+	buffer.WriteString(fmt.Sprintf("  suffix: %q\n", l.suffix))
+	buffer.WriteString(fmt.Sprintf("  separator: %q\n", l.separator))
+	buffer.WriteString(fmt.Sprintf("  extension: %q\n", l.extension))
+	buffer.WriteString(fmt.Sprintf("  layout: %s\n", l.layout))
+	buffer.WriteString(fmt.Sprintf("  format: %s\n", l.format))
+	buffer.WriteString(fmt.Sprintf("  current filename: %s\n", l.FileName()))
+
+	return strings.TrimSpace(buffer.String())
+}
+
+// canWrite tests if we have permissions to create/write files in l.path
+func (l *Logger) canWrite() error {
+	f, err := ioutil.TempFile(l.path, "gyro.Logger.")
+	if err != nil {
+		return err
+	}
+
+	f.Close()
+	return os.Remove(f.Name())
+}
+
+// buildFormatString precalculates the format string for the filenames
+// so we dont have to do the string interpolation on each call
+func (l *Logger) buildFormatString() {
+	p, s := len(l.prefix) > 0, len(l.suffix) > 0
+
+	if p && s {
+		l.format = strings.TrimSpace(fmt.Sprintf("%s%s%s%s%s.%s", l.prefix, l.separator, "%s", l.separator, l.suffix, l.extension))
+	} else if p && !s {
+		l.format = strings.TrimSpace(fmt.Sprintf("%s%s%s.%s", l.prefix, l.separator, "%s", l.extension))
+	} else if !p && s {
+		l.format = strings.TrimSpace(fmt.Sprintf("%s%s%s.%s", "%s", l.separator, l.suffix, l.extension))
+	} else {
+		l.format = strings.TrimSpace(fmt.Sprintf("%s.%s", "%s", l.extension))
+	}
+
+	if len(l.extension) == 0 {
+		l.format = l.format[:len(l.format)-1]
+	}
+}
---
+++ gyro_test.go
@@ -0,0 +1,156 @@
+package gyro
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateLogger(t *testing.T) {
+	var logger *Logger
+	var err error
+
+	logger, err = New("test")
+	assert.NotNil(t, logger)
+	assert.NoError(t, err)
+
+	logger, err = New("noexist")
+	assert.Nil(t, logger)
+	assert.Error(t, err)
+}
+
+func TestFilenames(t *testing.T) {
+	logger, err := New("test")
+	require.NotNil(t, logger)
+	require.NoError(t, err)
+
+	// We need a static time to work with for the tests
+	logger.SetTimeFunction(func() time.Time {
+		return time.Unix(0, 0).UTC()
+	})
+
+	var tests = []struct {
+		fn       func(string)
+		input    string
+		expected string
+	}{
+		{func(s string) {}, "", "1970-01-01T00.log"}, //default values test
+		{logger.SetSeparator, "_", "1970-01-01T00.log"},
+		{logger.SetExtension, "", "1970-01-01T00"},
+		{logger.SetExtension, "txt", "1970-01-01T00.txt"},
+		{logger.SetPrefix, "pre", "pre_1970-01-01T00.txt"},
+		{logger.SetSuffix, "suf", "pre_1970-01-01T00_suf.txt"},
+		{logger.SetLayout, "2006010215", "pre_1970010100_suf.txt"},
+	}
+
+	for _, test := range tests {
+		test.fn(test.input)
+		assert.Equal(t, test.expected, logger.FileName())
+	}
+
+	// Sanity check of the logger state
+	assert.Equal(t, "_", logger.separator)
+	assert.Equal(t, "txt", logger.extension)
+	assert.Equal(t, "pre", logger.prefix)
+	assert.Equal(t, "suf", logger.suffix)
+	assert.Equal(t, "2006010215", logger.layout)
+	assert.Equal(t, "pre_%s_suf.txt", logger.format)
+	assert.True(t, time.Unix(0, 0).UTC().Equal(logger.timeFn()))
+}
+
+func TestSetTime(t *testing.T) {
+	logger, err := New("test")
+	require.NotNil(t, logger)
+	require.NoError(t, err)
+
+	logger.SetTimeFunction(func() time.Time {
+		return time.Unix(0, 0).UTC()
+	})
+	assert.Equal(t, "1970-01-01T00.log", logger.FileName())
+
+	// Switch time zone to US/Alaska
+	logger.SetTimeFunction(func() time.Time {
+		loc, err := time.LoadLocation("US/Alaska")
+		assert.NoError(t, err)
+		return time.Unix(0, 0).UTC().In(loc)
+	})
+	assert.Equal(t, "1969-12-31T14.log", logger.FileName())
+
+	// Add one hour
+	logger.SetTimeFunction(func() time.Time {
+		loc, err := time.LoadLocation("US/Alaska")
+		assert.NoError(t, err)
+		return time.Unix(0, 0).UTC().In(loc).Add(1 * time.Hour)
+	})
+	assert.Equal(t, "1969-12-31T15.log", logger.FileName())
+}
+
+func TestWrites(t *testing.T) {
+	logger, err := New("test")
+	require.NotNil(t, logger)
+	require.NoError(t, err)
+	logger.SetTimeFunction(func() time.Time {
+		return time.Unix(0, 0)
+	})
+
+	fn := filepath.Join("test", logger.FileName())
+
+	// The test file should not be present at the start of the test
+	_, err = os.Stat(fn)
+	require.Error(t, err)
+
+	// Remove the test file after the tests
+	defer os.Remove(fn)
+
+	assert.NoError(t, logger.Write([]byte("test line 1\n")))
+	assert.NoError(t, logger.WriteString("test line 2\n"))
+
+	// Read in the file and check that the content matches what we just wrote
+	f, err := os.Open(fn)
+	assert.NoError(t, err)
+	contents, err := ioutil.ReadAll(f)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("test line 1\ntest line 2\n"), contents)
+}
+
+func BenchmarkWriteBytes(b *testing.B) {
+	logger, err := New("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger.SetTimeFunction(func() time.Time {
+		return time.Unix(0, 0)
+	})
+
+	fn := filepath.Join("test", logger.FileName())
+	defer os.Remove(fn)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		logger.Write([]byte("testing\n"))
+	}
+}
+
+func BenchmarkWriteStrings(b *testing.B) {
+	logger, err := New("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger.SetTimeFunction(func() time.Time {
+		return time.Unix(0, 0)
+	})
+
+	fn := filepath.Join("test", logger.FileName())
+	defer os.Remove(fn)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		logger.WriteString("testing\n")
+	}
+}
